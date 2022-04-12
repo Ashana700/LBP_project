69164bb0cd6837bce55248016099ce2d7928f8f2
Ashana700 <ashana_s@me.iitr.ac.in>
2022-04-13 02:29:58 +0530

    adding photos

--- src/components/faculties/Prof_7.jsx
+++ src/components/faculties/Prof_7.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Button_image from "../../images/button-image.png";
-import image from "../../images/Akshay_diwedi.jpg";
+import image from "../../images/vidit_gaur.jpg";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 
